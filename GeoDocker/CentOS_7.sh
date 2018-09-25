5d69722b0e56115f118613b496c6308bfe1b57c2
brownnrl <brownnrl@gmail.com>
2018-09-24 21:00:15 -0400

    comment out geodocker.

--- GeoDocker/CentOS_7.sh
+++ GeoDocker/CentOS_7.sh
@@ -115,7 +115,7 @@ echo "installing git..."
 yum install -y git
 
 echo "cloning geo-docker library and starting geomesa..."
-su $SSUSER -c 'cd ~; git clone https://github.com/geodocker/geodocker-geomesa.git; cd ~/geodocker-geomesa/geodocker-accumulo-geomesa; docker-compose up;"
+# su $SSUSER -c 'cd ~; git clone https://github.com/geodocker/geodocker-geomesa.git; cd ~/geodocker-geomesa/geodocker-accumulo-geomesa; docker-compose up;"
 
 #
 echo All finished!
