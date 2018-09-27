f78cb318752d98b3b1b17b706b16de96c8d8092d
brownnrl <brownnrl@gmail.com>
2018-09-26 21:06:51 -0400

    Add quick retreival of opensky data

--- GeoDocker/CentOS_7.sh
+++ GeoDocker/CentOS_7.sh
@@ -119,6 +119,12 @@ yum install -y git
 
 echo "cloning geo-docker library and starting geomesa..."
 su $SSUSER -c 'cd ~ && git clone https://github.com/geodocker/geodocker-geomesa.git && cd ~/geodocker-geomesa/geodocker-accumulo-geomesa && docker-compose pull;'
+echo "getting opensky data from Aug 18"
+su $SSUSER -c 'cd ~ && curl -o googledoc.py -L https://raw.githubusercontent.com/brownnrl/linux-deployment-scripts/master/GeoDocker/googledoc.py && chmod +x googledoc.py && ./googledoc.py 19GHBWBresVR6ZTfgRBQNzR_iIW-vXU3e opensky.csv.id'
+
+. ./out.sh
+
+. ./out.sh
 
 #
 echo All finished!
